6779ec5ff06c83811987ee5380453a770d652c17
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2018-09-06 12:45:10 +0200

    Dynamic graph based on Input User

--- Dash_3.py
+++ Dash_3.py
@@ -1,19 +1,117 @@
 import pandas as pd
 pd.core.common.is_list_like = pd.api.types.is_list_like
 import pandas_datareader.data as web
-import datetime
+import datetime as dt
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-start = datetime.datetime(2015, 1, 1)
-end = datetime.datetime.now()
+start = dt.datetime(2015, 1, 1)
+end = dt.datetime.now()
 
-df = web.DataReader('TSLA', 'morningstar', start, end)
-df.reset_index(inplace=True)
-df.set_index('Date', inplace=True)
-df = df.drop('Symbol', axis=1)
+stock = 'TSLA'
+df = web.DataReader(stock, 'iex', start, end)
+#df.reset_index(inplace=True)
+#df.set_index('Date', inplace=True)
+#df = df.drop('Symbol', axis=1)
 #print(df.head())
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph')
+    ])
+
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+    )
+def update_graph(input_data):
+    start = dt.datetime(2015, 1, 1)
+    end = dt.datetime.now()
+    df = web.DataReader(input_data, 'iex', start, end)
+    #df.reset_index(inplace=True)
+    #df.set_index('Date', inplace=True)
+    #df = df.drop('Symbol', axis=1)
+
+    return dcc.Graph(
+        id='example-grah',
+        figure={
+            'data': [
+                {'x': df.index, 'y': df['close'], 'type':'line', 'name': input_data}
+                ],
+            'layout': {
+                'title': input_data
+                }
+            })
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
